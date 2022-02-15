3a4cf30858fb645d4dfae204ec010b7f459cc645
DaniilGOK <gerasimenokd638@gmail.com>
2022-02-16 00:49:35 +0300

    feat: comlited revere-int task"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let revM = -n;
+    let revMinus = revM.toString().split("").reverse().join("");
+
+        if(+n < revMinus) return Number(revMinus);
+
+    let revN = n.toString().split("").reverse().join("");
+    let revNol = Number(revN);
+        if(n % 2 == 0) return revNol;
+    let rev = n.toString().split("").reverse().join("");
+        if(n) return Number(rev);
 }
